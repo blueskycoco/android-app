9553fb818d877e407176f7c8fdd1546c6f15baab
Your Name <you@example.com>
2016-02-20 11:41:14 +0800

    add com.project.water auto startup

--- water/src/com/example/water/MainActivity.java
+++ water/src/com/example/water/MainActivity.java
@@ -505,25 +505,5 @@ public class MainActivity extends Activity {
                     }
             });
     }
-	public class BootBroadCast extends BroadcastReceiver {  
-	    @Override  
-	    public void onReceive(Context context, Intent intent) {  
-	        /*  
-	         * 开机启动服务*  
-	         * Intent service=new Intent(context, MyService.class);  
-	         * context.startService(service);  
-	         */  
-	  
-	        /*  
-	         * 开机启动的Activity*  
-	         * Intent activity=new Intent(context,MyActivity.class);  
-	         * activity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK );//不加此句会报错。  
-	         * context.startActivity(activity);  
-	         */  
-	  
-	        /* 开机启动的应用 */  
-	        Intent appli = context.getPackageManager().getLaunchIntentForPackage("com.example.water");  
-	        context.startActivity(appli);  
-	    }
-	}
+	
 }
